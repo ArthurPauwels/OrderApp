df714359e9bdbcaf4907e527d1d2dab11a42f8d9
ArthurPauwels <arthur.pauwels@student.hogent.be>
2020-07-25 17:01:31 +0200

    added navigation drawer and back button

--- app/src/main/java/com/example/orderapp/MainActivity.kt
+++ app/src/main/java/com/example/orderapp/MainActivity.kt
@@ -3,12 +3,26 @@ package com.example.orderapp
 import androidx.databinding.DataBindingUtil
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
 import com.example.orderapp.databinding.ActivityMainBinding
 
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var drawerLayout : DrawerLayout
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+        val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+        drawerLayout = binding.drawerLayout
+        val navController = this.findNavController(R.id.navHostFragment)
+        NavigationUI.setupActionBarWithNavController(this, navController, drawerLayout)
+        NavigationUI.setupWithNavController(binding.navView, navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = this.findNavController(R.id.navHostFragment)
+        return NavigationUI.navigateUp(navController, drawerLayout)
     }
 }
--- app/src/main/java/com/example/orderapp/fragments/BusinessOverview.kt
+++ app/src/main/java/com/example/orderapp/fragments/BusinessOverview.kt
@@ -3,10 +3,10 @@ package com.example.orderapp.fragments
 import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
+import android.view.Menu
 import android.widget.Toast
+import androidx.core.app.ShareCompat
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
@@ -19,7 +19,7 @@ class BusinessOverview : Fragment() {
 
     lateinit var binding: FragmentBusinessOverviewBinding
 
-    private var businessID : String = ""
+    private var businessID : String? = null
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -33,11 +33,41 @@ class BusinessOverview : Fragment() {
         //setup data
         binding.business = Business()
         //setup buttons
-        binding.btnScanCode.setOnClickListener { clickBttnScanCode() }
         binding.bttnOrder.setOnClickListener { clickBttnOrder() }
         binding.bttnOrder.visibility = View.GONE
 
+        setHasOptionsMenu(true)
+
         return binding.root
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater?.inflate(R.menu.business_overview_menu, menu)
+        //disable share option if needed
+        if (getShareIntent().resolveActivity(requireActivity().packageManager) == null) disableShareOption(menu)
+        if (businessID == null) disableShareOption(menu)
+    }
+
+    private fun disableShareOption(menu: Menu){
+        menu.findItem(R.id.shareBusiness).setVisible(false)
+    }
+
+    private fun getShareIntent() : Intent {
+        //TODO add better sharing text things
+        return ShareCompat.IntentBuilder.from(requireActivity()).setText("placeholder Sharing Text that should be replaced soon").setType("text/plain").intent
+    }
+
+    private fun shareBusiness(){
+        startActivity(getShareIntent())
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.shareBusiness -> shareBusiness()
+            R.id.scanCode -> scanCode()
+        }
+        return super.onOptionsItemSelected(item)
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
@@ -57,10 +87,10 @@ class BusinessOverview : Fragment() {
 
     private fun clickBttnOrder(){
         //view?.findNavController()?.navigate(BusinessOverviewDirections.actionBusinessOverviewToBusinessNotOpen())
-        view?.findNavController()?.navigate(BusinessOverviewDirections.actionBusinessOverviewToMenu(businessID, binding.business!!.name))
+        view?.findNavController()?.navigate(BusinessOverviewDirections.actionBusinessOverviewToMenu(businessID!!, binding.business!!.name))
     }
 
-    private fun clickBttnScanCode(){
+    private fun scanCode(){
         //setup scanner
         val integrator = IntentIntegrator.forSupportFragment(this)
         integrator.setDesiredBarcodeFormats(IntentIntegrator.QR_CODE)
@@ -71,8 +101,10 @@ class BusinessOverview : Fragment() {
 
     private fun codeScanned(code : String){
         binding.apply {
-            //get info from model
-            business?.name = code
+            //TODO parse code into business and table
+            //TODO get info from repo instead
+            businessID = code
+            business?.name = "Business with id ${code}"
             business?.type = "Bruh"
             invalidateAll()
             bttnOrder.visibility = View.VISIBLE
