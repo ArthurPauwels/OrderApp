36a310b595c3d37c0e74a2044a020fc052159a39
ArthurPauwels <arthur.pauwels@student.hogent.be>
2020-07-30 17:40:11 +0200

    categories custom view holder

--- app/src/main/java/com/example/orderapp/fragments/menu/MenuAdapter.kt
+++ app/src/main/java/com/example/orderapp/fragments/menu/MenuAdapter.kt
@@ -1,31 +1,33 @@
 package com.example.orderapp.fragments.menu
 
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.orderapp.R
 import com.example.orderapp.domain.MenuCategory
 
-class MenuAdapter : RecyclerView.Adapter<TextItemViewHolder>() {
+class MenuAdapter : RecyclerView.Adapter<MenuAdapter.ViewHolder>() {
     var data = listOf<MenuCategory>()
     set(value) {
         field = value
         notifyDataSetChanged()
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TextItemViewHolder {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MenuAdapter.ViewHolder {
         val layoutInflater = LayoutInflater.from(parent.context)
-        val view = layoutInflater.inflate(R.layout.text_item_view, parent, false) as TextView
-        return TextItemViewHolder(view)
+        val view = layoutInflater.inflate(R.layout.list_item_category, parent, false)
+        return ViewHolder(view)
     }
 
     override fun getItemCount() = data.size
 
-    override fun onBindViewHolder(holder: TextItemViewHolder, position: Int) {
-        holder.textView.text = data[position].name
+    override fun onBindViewHolder(holder: MenuAdapter.ViewHolder, position: Int) {
+        holder.name.text = data[position].name
+    }
+    class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView){
+        val name : TextView = itemView.findViewById(R.id.txt_category_name)
     }
 
 }
-
-class TextItemViewHolder(val textView : TextView) : RecyclerView.ViewHolder(textView)
